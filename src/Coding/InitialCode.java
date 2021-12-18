63fa68b4495ebdec6e3f0eaa8f53ec1fce83992c
Priyanka <priyankasingla221@gmail.com>
2021-12-18 14:09:26 -0400

    calculator

---
+++ src/Coding/InitialCode.java
@@ -0,0 +1,20 @@
+package Coding;
+
+public class InitialCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1= 1 ;
+		int num2= 2 ;
+		int Resultadd= num1 + num2;
+		int Resultsub= num1 - num2;
+		int ResultMulti= num1 * num2;
+		System.out.println("First Number= " +num1);
+		System.out.println("Second Number= " +num2);
+		System.out.println("Addition Result = " +Resultadd);
+		System.out.println("Subtraction Result = " +Resultsub);
+		System.out.println("Multiplication Result " +ResultMulti);	
+		
+	}
+
+}
